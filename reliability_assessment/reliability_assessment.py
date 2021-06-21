04307186ab44761d7902135418daf4e61240ec0e
Bill Chan <billpwchan@hotmail.com>
2021-06-21 15:50:09 +0800

    [RA] Update GLTR

--- reliability_assessment/reliability_assessment.py
+++ reliability_assessment/reliability_assessment.py
@@ -523,7 +523,7 @@ class ReliabilityAssessment:
         if gltr_gpt2 or gltr_bert:
             gltr_type = DETECTOR_MAP['gltr-detector'][0] if gltr_gpt2 else DETECTOR_MAP['gltr-detector'][1]
             self.nv_instance.init_gltr_models(model=gltr_type)
-            SLICES = 4
+            SLICES = 50
             if fake:
                 gltr_collection = self.db_instance.get_neural_non_updated_tweets(
                     f"ra_raw.{gltr_type}-detector",
