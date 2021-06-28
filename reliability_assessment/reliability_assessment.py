b59f8b95c917f843ba3fb6088aaf21196ed692ad
Bill Chan <billpwchan@hotmail.com>
2021-06-28 10:47:09 +0100

    [RA] Update Infersent

--- reliability_assessment/reliability_assessment.py
+++ reliability_assessment/reliability_assessment.py
@@ -12,10 +12,12 @@ from datetime import date
 from random import randint
 
 import joblib
+import nltk
 import numpy as np
 import pandas as pd
 import requests
 import torch
+import torch.nn as nn
 from profanity_check import predict_prob
 from sklearn import preprocessing
 from sklearn.metrics import classification_report
@@ -37,6 +39,7 @@ from util import *
 
 patch_sklearn()
 gc.enable()
+nltk.download('punkt')
 
 SUB_PROCESSES = []
 
@@ -47,6 +50,8 @@ DETECTOR_MAP = {
     'gpt-detector-server':  'http://localhost:8080/',
     'gltr-detector-server': ('http://localhost:5001/', 'http://localhost:5002/')
 }
+
+PATH_RA = './reliability_assessment'
 
 
 class TweetGeneration:
@@ -272,8 +277,8 @@ class NeuralVerifier:
 
     def init_gpt_model(self, model: str = DETECTOR_MAP['gpt-detector']):
         self.default_logger.info("Initialize GPT-2 Neural Verifier")
-        gpt_2_server = subprocess.Popen(["python", "./reliability_assessment/roberta_detector/server.py",
-                                         f"./reliability_assessment/roberta_detector/models/{model}"])
+        gpt_2_server = subprocess.Popen(["python", f"{PATH_RA}/roberta_detector/server.py",
+                                         f"{PATH_RA}/roberta_detector/models/{model}"])
         SUB_PROCESSES.append(gpt_2_server)
         while True:
             try:
@@ -290,7 +295,7 @@ class NeuralVerifier:
         default_port = DETECTOR_MAP['gltr-detector-server'][DETECTOR_MAP['gltr-detector'].index(model)][-5:-1]
         self.default_logger.info(f"Initialize GLTR {model}")
         gltr_gpt_server = subprocess.Popen(
-            ["python", "./reliability_assessment/gltr/server.py", "--model", f"{model}", "--port",
+            ["python", f"{PATH_RA}/gltr/server.py", "--model", f"{model}", "--port",
              f"{default_port}"])
         SUB_PROCESSES.append(gltr_gpt_server)
         while True:
@@ -303,7 +308,7 @@ class NeuralVerifier:
 
     def __download_models(self, mode: str = 'gpt-2'):
         if mode == 'gpt-2':
-            dir_prefix = "./reliability_assessment/roberta_detector/models/"
+            dir_prefix = f"{PATH_RA}/roberta_detector/models/"
             base_model = pathlib.Path(f'{dir_prefix}detector-base.pt')
             if not base_model.exists():
                 open(f'{dir_prefix}detector-base.pt', 'wb').write(
@@ -321,7 +326,7 @@ class NeuralVerifier:
             else:
                 self.default_logger.info(f'{mode} large model exists')
         if mode == 'grover':
-            dir_prefix = "./reliability_assessment/grover/models/mega-0.94/"
+            dir_prefix = f"{PATH_RA}/grover/models/mega-0.94/"
             model_type = 'discrimination'
             for ext in ['data-00000-of-00001', 'index', 'meta']:
                 model_path = pathlib.Path(f'{dir_prefix}model.ckpt-1562.{ext}')
@@ -399,6 +404,255 @@ class NeuralVerifier:
             return output_data
         else:
             raise NotImplementedError
+
+
+class InferSent(nn.Module):
+
+    def __init__(self, config):
+        super(InferSent, self).__init__()
+        self.bsize = config['bsize']
+        self.word_emb_dim = config['word_emb_dim']
+        self.enc_lstm_dim = config['enc_lstm_dim']
+        self.pool_type = config['pool_type']
+        self.dpout_model = config['dpout_model']
+        self.version = 1 if 'version' not in config else config['version']
+
+        self.enc_lstm = nn.LSTM(self.word_emb_dim, self.enc_lstm_dim, 1,
+                                bidirectional=True, dropout=self.dpout_model)
+
+        assert self.version in [1, 2]
+        if self.version == 1:
+            self.bos = '<s>'
+            self.eos = '</s>'
+            self.max_pad = True
+            self.moses_tok = False
+        elif self.version == 2:
+            self.bos = '<p>'
+            self.eos = '</p>'
+            self.max_pad = False
+            self.moses_tok = True
+
+    def is_cuda(self):
+        # either all weights are on cpu or they are on gpu
+        return self.enc_lstm.bias_hh_l0.data.is_cuda
+
+    def forward(self, sent_tuple):
+        # sent_len: [max_len, ..., min_len] (bsize)
+        # sent: (seqlen x bsize x worddim)
+        sent, sent_len = sent_tuple
+
+        # Sort by length (keep idx)
+        sent_len_sorted, idx_sort = np.sort(sent_len)[::-1], np.argsort(-sent_len)
+        sent_len_sorted = sent_len_sorted.copy()
+        idx_unsort = np.argsort(idx_sort)
+
+        idx_sort = torch.from_numpy(idx_sort).cuda() if self.is_cuda() \
+            else torch.from_numpy(idx_sort)
+        sent = sent.index_select(1, idx_sort)
+
+        # Handling padding in Recurrent Networks
+        sent_packed = nn.utils.rnn.pack_padded_sequence(sent, sent_len_sorted)
+        sent_output = self.enc_lstm(sent_packed)[0]  # seqlen x batch x 2*nhid
+        sent_output = nn.utils.rnn.pad_packed_sequence(sent_output)[0]
+
+        # Un-sort by length
+        idx_unsort = torch.from_numpy(idx_unsort).cuda() if self.is_cuda() \
+            else torch.from_numpy(idx_unsort)
+        sent_output = sent_output.index_select(1, idx_unsort)
+
+        # Pooling
+        if self.pool_type == "mean":
+            sent_len = torch.FloatTensor(sent_len.copy()).unsqueeze(1).cuda()
+            emb = torch.sum(sent_output, 0).squeeze(0)
+            emb = emb / sent_len.expand_as(emb)
+        elif self.pool_type == "max":
+            if not self.max_pad:
+                sent_output[sent_output == 0] = -1e9
+            emb = torch.max(sent_output, 0)[0]
+            if emb.ndimension() == 3:
+                emb = emb.squeeze(0)
+                assert emb.ndimension() == 2
+
+        return emb
+
+    def set_w2v_path(self, w2v_path):
+        self.w2v_path = w2v_path
+
+    def get_word_dict(self, sentences, tokenize=True):
+        # create vocab of words
+        word_dict = {}
+        sentences = [s.split() if not tokenize else self.tokenize(s) for s in sentences]
+        for sent in sentences:
+            for word in sent:
+                if word not in word_dict:
+                    word_dict[word] = ''
+        word_dict[self.bos] = ''
+        word_dict[self.eos] = ''
+        return word_dict
+
+    def get_w2v(self, word_dict):
+        assert hasattr(self, 'w2v_path'), 'w2v path not set'
+        # create word_vec with w2v vectors
+        word_vec = {}
+        with open(self.w2v_path, encoding='utf-8') as f:
+            for line in f:
+                word, vec = line.split(' ', 1)
+                if word in word_dict:
+                    word_vec[word] = np.fromstring(vec, sep=' ')
+        print('Found %s(/%s) words with w2v vectors' % (len(word_vec), len(word_dict)))
+        return word_vec
+
+    def get_w2v_k(self, K):
+        assert hasattr(self, 'w2v_path'), 'w2v path not set'
+        # create word_vec with k first w2v vectors
+        k = 0
+        word_vec = {}
+        with open(self.w2v_path, encoding='utf-8') as f:
+            for line in f:
+                word, vec = line.split(' ', 1)
+                if k <= K:
+                    word_vec[word] = np.fromstring(vec, sep=' ')
+                    k += 1
+                if k > K:
+                    if word in [self.bos, self.eos]:
+                        word_vec[word] = np.fromstring(vec, sep=' ')
+
+                if k > K and all([w in word_vec for w in [self.bos, self.eos]]):
+                    break
+        return word_vec
+
+    def build_vocab(self, sentences, tokenize=True):
+        assert hasattr(self, 'w2v_path'), 'w2v path not set'
+        word_dict = self.get_word_dict(sentences, tokenize)
+        self.word_vec = self.get_w2v(word_dict)
+        print('Vocab size : %s' % (len(self.word_vec)))
+
+    # build w2v vocab with k most frequent words
+    def build_vocab_k_words(self, K):
+        assert hasattr(self, 'w2v_path'), 'w2v path not set'
+        self.word_vec = self.get_w2v_k(K)
+        print('Vocab size : %s' % (K))
+
+    def update_vocab(self, sentences, tokenize=True):
+        assert hasattr(self, 'w2v_path'), 'warning : w2v path not set'
+        assert hasattr(self, 'word_vec'), 'build_vocab before updating it'
+        word_dict = self.get_word_dict(sentences, tokenize)
+
+        # keep only new words
+        for word in self.word_vec:
+            if word in word_dict:
+                del word_dict[word]
+
+        # udpate vocabulary
+        if word_dict:
+            new_word_vec = self.get_w2v(word_dict)
+            self.word_vec.update(new_word_vec)
+        else:
+            new_word_vec = []
+        print('New vocab size : %s (added %s words)' % (len(self.word_vec), len(new_word_vec)))
+
+    def get_batch(self, batch):
+        # sent in batch in decreasing order of lengths
+        # batch: (bsize, max_len, word_dim)
+        embed = np.zeros((len(batch[0]), len(batch), self.word_emb_dim))
+
+        for i in range(len(batch)):
+            for j in range(len(batch[i])):
+                embed[j, i, :] = self.word_vec[batch[i][j]]
+
+        return torch.FloatTensor(embed)
+
+    def tokenize(self, s):
+        from nltk.tokenize import word_tokenize
+        if self.moses_tok:
+            s = ' '.join(word_tokenize(s))
+            s = s.replace(" n't ", "n 't ")  # HACK to get ~MOSES tokenization
+            return s.split()
+        else:
+            return word_tokenize(s)
+
+    def prepare_samples(self, sentences, bsize, tokenize, verbose):
+        sentences = [[self.bos] + s.split() + [self.eos] if not tokenize else
+                     [self.bos] + self.tokenize(s) + [self.eos] for s in sentences]
+        n_w = np.sum([len(x) for x in sentences])
+
+        # filters words without w2v vectors
+        for i in range(len(sentences)):
+            s_f = [word for word in sentences[i] if word in self.word_vec]
+            if not s_f:
+                import warnings
+                warnings.warn('No words in "%s" (idx=%s) have w2v vectors. \
+                               Replacing by "</s>"..' % (sentences[i], i))
+                s_f = [self.eos]
+            sentences[i] = s_f
+
+        lengths = np.array([len(s) for s in sentences])
+        n_wk = np.sum(lengths)
+        if verbose:
+            print('Nb words kept : %s/%s (%.1f%s)' % (
+                n_wk, n_w, 100.0 * n_wk / n_w, '%'))
+
+        # sort by decreasing length
+        lengths, idx_sort = np.sort(lengths)[::-1], np.argsort(-lengths)
+        sentences = np.array(sentences)[idx_sort]
+
+        return sentences, lengths, idx_sort
+
+    def encode(self, sentences, bsize=64, tokenize=True, verbose=False):
+        tic = time.time()
+        sentences, lengths, idx_sort = self.prepare_samples(
+            sentences, bsize, tokenize, verbose)
+
+        embeddings = []
+        for stidx in range(0, len(sentences), bsize):
+            batch = self.get_batch(sentences[stidx:stidx + bsize])
+            if self.is_cuda():
+                batch = batch.cuda()
+            with torch.no_grad():
+                batch = self.forward((batch, lengths[stidx:stidx + bsize])).data.cpu().numpy()
+            embeddings.append(batch)
+        embeddings = np.vstack(embeddings)
+
+        # unsort
+        idx_unsort = np.argsort(idx_sort)
+        embeddings = embeddings[idx_unsort]
+
+        if verbose:
+            print('Speed : %.1f sentences/s (%s mode, bsize=%s)' % (
+                len(embeddings) / (time.time() - tic),
+                'gpu' if self.is_cuda() else 'cpu', bsize))
+        return embeddings
+
+    def visualize(self, sent, tokenize=True):
+
+        sent = sent.split() if not tokenize else self.tokenize(sent)
+        sent = [[self.bos] + [word for word in sent if word in self.word_vec] + [self.eos]]
+
+        if ' '.join(sent[0]) == '%s %s' % (self.bos, self.eos):
+            import warnings
+            warnings.warn('No words in "%s" have w2v vectors. Replacing \
+                           by "%s %s"..' % (sent, self.bos, self.eos))
+        batch = self.get_batch(sent)
+
+        if self.is_cuda():
+            batch = batch.cuda()
+        output = self.enc_lstm(batch)[0]
+        output, idxs = torch.max(output, 0)
+        # output, idxs = output.squeeze(), idxs.squeeze()
+        idxs = idxs.data.cpu().numpy()
+        argmaxs = [np.sum((idxs == k)) for k in range(len(sent[0]))]
+
+        # visualize model
+        import matplotlib.pyplot as plt
+        x = range(len(sent[0]))
+        y = [100.0 * n / np.sum(argmaxs) for n in argmaxs]
+        plt.xticks(x, sent[0], rotation=45)
+        plt.bar(x, y)
+        plt.ylabel('%')
+        plt.title('Visualisation of words importance')
+        plt.show()
+
+        return output, idxs
 
 
 class ReliabilityAssessment:
@@ -565,7 +819,7 @@ class ReliabilityAssessment:
 
         if classifier:
             for gltr_type in DETECTOR_MAP['gltr-detector']:
-                file_path = f'./reliability_assessment/neural_classifier/{self.ticker}_{self.input_date}_{gltr_type}_svm.pkl'
+                file_path = f'{PATH_RA}/neural_classifier/{self.ticker}_{self.input_date}_{gltr_type}_svm.pkl'
                 file_path_check = Path(file_path)
                 if file_path_check.is_file():
                     clf = joblib.load(file_path)
@@ -604,17 +858,17 @@ class ReliabilityAssessment:
 
         train, test = np.split(np.array(tweets_collection), [int(len(tweets_collection) * 0.8)])
         for index, value in {'train': train, 'test': test}.items():
-            with open(f'./reliability_assessment/detector_dataset/{self.ticker}_{self.input_date}_{index}.txt', 'w+',
+            with open(f'{PATH_RA}/detector_dataset/{self.ticker}_{self.input_date}_{index}.txt', 'w+',
                       encoding='utf-8') as file_handle:
                 file_handle.writelines(f"{tweet}\n" for tweet in value)
 
     def neural_fake_news_generator_fine_tune(self, model_type, model_name_or_path):
         gpt_2_fine_tune = subprocess.call(
-            ["python", "./reliability_assessment/run_clm.py", "--model_type", "gpt2",
+            ["python", f"{PATH_RA}/run_clm.py", "--model_type", "gpt2",
              "--model_name_or_path", "gpt2-medium",
              "--train_data_file",
-             f"./reliability_assessment/detector_dataset/{self.ticker}_{self.input_date}_train.txt",
-             "--eval_data_file", f"./reliability_assessment/detector_dataset/{self.ticker}_{self.input_date}_test.txt",
+             f"{PATH_RA}/detector_dataset/{self.ticker}_{self.input_date}_train.txt",
+             "--eval_data_file", f"{PATH_RA}/detector_dataset/{self.ticker}_{self.input_date}_test.txt",
              "--line_by_line", "--do_train", "--do_eval", "--output_dir", "./gpt_generator", "--overwrite_output_dir",
              "--per_gpu_train_batch_size", "1", "--per_gpu_eval_batch_size", "1", "--learning_rate", "5e-5",
              "--save_steps", "50000", "--logging_steps", "50", "--num_train_epochs", "1"])
@@ -695,7 +949,7 @@ class ReliabilityAssessment:
 
         df = pd.DataFrame(X)
         df['y'] = y
-        df.to_csv(f'./reliability_assessment/detector_dataset/{self.ticker}_{self.input_date}_{gltr_type}.csv')
+        df.to_csv(f'{PATH_RA}/detector_dataset/{self.ticker}_{self.input_date}_{gltr_type}.csv')
 
         le = preprocessing.LabelEncoder()
         le.fit(y)
@@ -725,8 +979,18 @@ class ReliabilityAssessment:
             clf = SVC(C=1000, gamma=1, kernel='rbf')
 
         clf.fit(X, y)
-        joblib.dump(clf,
-                    f'./reliability_assessment/neural_classifier/{self.ticker}_{self.input_date}_{gltr_type}_svm.pkl')
+        joblib.dump(clf, f'{PATH_RA}/neural_classifier/{self.ticker}_{self.input_date}_{gltr_type}_svm.pkl')
 
     def neural_fake_news_verify(self):
         print("Let's Verify")
+
+    def subjectivity_sentence_emb(self):
+        V = 2
+        MODEL_PATH = f'encoder/infersent{V}.pkl'
+        params_model = {'bsize':     64, 'word_emb_dim': 300, 'enc_lstm_dim': 2048,
+                        'pool_type': 'max', 'dpout_model': 0.0, 'version': V}
+        infersent = InferSent(params_model)
+        infersent.load_state_dict(torch.load(MODEL_PATH))
+
+        W2V_PATH = f'{PATH_RA}/infersent/fastText/crawl-300d-2M.vec' if V == 2 else f'{PATH_RA}/infersent/GloVe/glove.840B.300d.txt'
+        infersent.set_w2v_path(W2V_PATH)
