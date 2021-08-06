3c2b0d03100a21c1a41174bbe6e19ba3bcc1855c
Bill Chan <billpwchan@hotmail.com>
2021-08-06 01:30:30 +0100

    [RA] Update Eval Script
    Signed-off-by: Bill Chan <billpwchan@hotmail.com>

--- reliability_assessment/reliability_assessment.py
+++ reliability_assessment/reliability_assessment.py
@@ -859,14 +859,13 @@ class ReliabilityAssessment:
 
     def tweet_eval(self):
         query_field = self.__annotation_query(self.ticker)
-        print(query_field)
         filters = ['feature-filter', 'neural-filter', 'arg-filter', 'subj-filter', 'label']
         projection_filed = {f'ra_raw.{filter_name}': 1 for filter_name in filters}
         label_dataset = self.db_instance.get_annotated_tweets(query_field, self.input_date, self.ticker,
                                                               projection_override=projection_filed)
 
         eval_df = pd.DataFrame([item['ra_raw'] for item in label_dataset], columns=filters)
-        print(eval_df.isna().sum())
+        self.default_logger.info(f'Missing Value (NaN) Summary: {eval_df.isna().sum()}')
         eval_df.fillna(False, inplace=True)
 
         eval_dict = {
